1ec739a2ce27ad6a9615ed4db1a62a2e0a58e586
viviniko <viviniko.shop@gmail.com>
2019-07-04 14:55:29 +0800

    :trollface

--- src/Console/Commands/MappingCommand.php
+++ src/Console/Commands/MappingCommand.php
@@ -53,9 +53,7 @@ class MappingCommand extends Command
                             'number_of_shards' => 1,
                             'number_of_replicas' => 0,
                         ],
-                        'mappings' => [
-                            $model->searchableAs() => $model->searchableMapping(),
-                        ],
+                        'mappings' => $model->searchableMapping(),
                     ],
                 ]);
             }
